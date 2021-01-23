61d25de3b455a21bac7d30362a20cf46dec83682
TateStaples <56731352+TateStaples@users.noreply.github.com>
2021-01-23 13:49:52 -0500

    Some stuff

---
+++ Reinforcement/general.py
@@ -0,0 +1,19 @@
+class GameState:
+    def __init__(self):
+        self.done = False
+
+    def get_actions(self):
+        pass
+
+    def encode(self):
+        pass
+
+    # q learing
+    def num_states(self):
+        pass
+
+    def index(self):
+        pass
+
+    def update(self, action):
+        pass
